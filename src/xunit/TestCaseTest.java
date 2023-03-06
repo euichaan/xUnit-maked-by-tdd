d13cc05f0db44d66ea362134fb3cfdd3e57f1729
euichanhwang <pine6971@naver.com>
2023-03-06 23:26:16 +0900

    feat : TestSuite을 이용해서 테스트 결과를 한번에 받아온다

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -7,13 +7,15 @@ public class TestCaseTest extends TestCase {
 
 	public void testTemplateMethod() {
 		WasRun wasRun = new WasRun("testMethod");
-		wasRun.run();
+		TestResult result = new TestResult();
+		wasRun.run(result);
 		Assert.assertEquals("setUp testMethod tearDown", wasRun.log);
 	}
 
 	public void testResult() {
 		WasRun wasRun = new WasRun("testMethod");
-		TestResult result = wasRun.run();
+		TestResult result = new TestResult();
+		wasRun.run(result);
 		Assert.assertEquals("1 run, 0 failed", result.getSummary());
 	}
 
@@ -28,7 +30,17 @@ public class TestCaseTest extends TestCase {
 	public void testFailedResult() {
 		// 실패하는 테스트로 Result를 받아오는 지 확인
 		WasRun wasRun = new WasRun("testBrokenMethod");
-		TestResult result = wasRun.run();
+		TestResult result = new TestResult();
+		wasRun.run(result);
 		Assert.assertEquals("1 run, 1 failed", result.getSummary());
+	}
+
+	public void testSuite() {
+		TestSuite suite = new TestSuite();
+		suite.add(new WasRun("testMethod"));
+		suite.add(new WasRun("testBrokenMethod"));
+		TestResult result = new TestResult(); // 각각 run을 호출하지 않아도 suite에서 한번에 run 할 수 있도록
+		suite.run(result);
+		Assert.assertEquals("2 run, 1 failed", result.getSummary());
 	}
 }
