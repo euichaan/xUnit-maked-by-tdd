1cd803922f7ad97857baf1f5f9cdcfa03ed9fd04
euichanhwang <pine6971@naver.com>
2023-03-06 23:46:08 +0900

    feat : Composite pattern을 이용해서 testSuite를 중첩할 수 있다

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -6,5 +6,13 @@ public class XUnitTest {
 		TestResult result = new TestResult(); // 결과 수집
 		suite.run(result);
 		System.out.println(result.getSummary());
+
+		// TestSuite 중첩 = Composite pattern
+		TestSuite suite2 = new TestSuite();
+		suite2.add(new TestCaseTest("testTemplateMethod"));
+		suite2.add(suite);
+		TestResult result2 = new TestResult(); // 결과 수집
+		suite2.run(result2);
+		System.out.println(result2.getSummary());
 	}
 }
